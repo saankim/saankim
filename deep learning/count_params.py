171f4a199baeb36462563c7b1b20e9dbe972cc28
San Kim <kssan99@gmail.com>
2022-04-12 20:20:32 +0900

    parameters 폴더 만듦

--- deep learning/count_params.py
+++
@@ -1,8 +0,0 @@
-# Keras
-model.count_params()
-
-# PyTorch
-pytorch_total_params = sum(p.numel() for p in model.parameters())
-
-## PyTorch only trainable parameters
-pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
